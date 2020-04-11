0b21a0ab092db006cb8c37aebc4525b5c676e71b
Tristan Marsell <pdesire@thepublictransport.de>
2020-04-11 13:22:00 +0200

    Fix title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Portfolio Tristan Marsell',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
